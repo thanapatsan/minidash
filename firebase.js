578163fa1b9825e93b10db8f12f4767eb5485d78
Flukekie <thanapatsan@protonmail.com>
2023-07-22 10:55:27 +0700

    init firebase

---
+++ firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+const initapp = () => {
+  try {
+    return getApp();
+  } catch {
+    return initializeApp(config);
+  }
+};
+
+let app = initapp();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,28 @@ export default function Index() {
   return (
     <main className="h-screen bg-slate-200 flex justify-center items-center">
       <div className="bg-slate-50 max-w-screen-md p-24">
-        <div className="max-w-xs">
-          <h1 className="text-4xl text-center mb-6">Welcome </h1>
-          <input
-            type="email"
-            placeholder="Email"
-            className="input input-bordered w-full "
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="input input-bordered w-full mt-2"
-          />
-          <button className="btn btn-primary w-full mt-6">login</button>
-          <a className="btn btn-link w-full mt-2">forgot password?</a>
+        <div className="w-64">
+          <h1 className="text-4xl text-center mb-6">Dashboard</h1>
+          <form className="form-control gap-2 ">
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              id="username"
+              className="input input-bordered w-full"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              id="password"
+              className="input input-bordered w-full"
+            />
+          </form>
+          <div className="gap-1 mt-6">
+            <button className="btn btn-primary w-full">login</button>
+            <a className="btn btn-link w-full mt-2">forgot password?</a>
+          </div>
         </div>
       </div>
     </main>
